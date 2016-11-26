bb5a2d92e7b4d21a9246c83c0bf823afdf8ae130
mattyarbrough <matt.yarbrough@dotcms.com>
2016-11-26 10:45:08 -0600

    changed to be consistent with model

--- admin.py
+++ admin.py
@@ -6,7 +6,7 @@ class CourseAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['session_id','course_name', 'course_slug', 'course_instructor', 'course_category_id', 'course_description']}),
         ('Date information', {'fields': ['course_start_time', 'course_end_time', 'course_days_of_week', 'course_all_day', 'custom_start_date', 'custom_end_date']}),
-        ('Logistics', {'fields': ['course_private', 'course_spaces', 'course_price', 'location_id', 'recurs', 'recurs_interval', 'recurs_times']}),
+        ('Logistics', {'fields': ['course_private', 'course_spaces', 'course_price', 'location_id', 'recurs', 'recurs_interval', 'custom_recurs_times']}),
     ]
 
 admin.site.register(Course, CourseAdmin)
