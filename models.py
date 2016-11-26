eb325c2fc154560404d5c6b131bd84562f97a470
mattyarbrough <matt.yarbrough@dotcms.com>
2016-11-26 10:30:14 -0600

    Update models.py
    Course locations may have a room number/building number/etc, added address2

--- models.py
+++ models.py
@@ -52,6 +52,7 @@ class Location(models.Model):
     location_name = models.CharField(blank=False, null=False, max_length=250)
     location_slug = models.CharField(max_length=100, blank=False, null=False)
     location_address = models.CharField(max_length=200, blank=False, null=False)
+    location_address2 = models.CharField(max_length=200, blank=True, null=True)
     location_city = models.CharField(max_length=200, blank=False, null=False)
     location_state = models.CharField(max_length=2, blank=False, null=False)
     location_postcode = models.CharField(max_length=10, blank=False, null=False)
